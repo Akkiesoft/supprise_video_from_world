e40bc801bc12eea0ac1495581686185da6da371e
Akira Ouchi <akkiesoft@marokun.net>
2022-12-06 14:52:05 +0900

    ﾝﾏｯ！

---
+++ README.md
@@ -0,0 +1,16 @@
+# mikutter世界のサプライズ動画プラグイン
+
+投稿ボックスのメッセージを世界のサプライズ動画のセリフっぽい感じに整えるmikutterのクソプラグインです。
+
+## つかいかた
+
+* インストール
+
+```
+git clone https://github.com/Akkiesoft/supprise_video_from_world ~/.mikutter/plugin/supprise_video_from_world
+```
+
+* 投稿ボックスに文字を入力
+    * 一言ずつで改行してください
+* プラグインを実行
+    * すぐ投稿するモードと、投稿ボックス内で整えるだけのモードがあります
---
+++ supprise_video_from_world.rb
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+Plugin.create :supprise_video_from_world do
+    command(:supprise_video_from_world,
+        name: '世界のサプライズ動画を作る',
+        condition: lambda{ |opt| true },
+        visible: true,
+        role: :postbox
+    ) do |opt|
+        Plugin.call(:create_supprise_video_from_world, opt, false)
+    end
+    command(:supprise_video_from_world_sendnow,
+        name: '世界のサプライズ動画を作ってすぐ送る',
+        condition: lambda{ |opt| true },
+        visible: true,
+        role: :postbox
+    ) do |opt|
+        Plugin.call(:create_supprise_video_from_world, opt, true)
+    end
+
+    on_create_supprise_video_from_world do |opt, send_now|
+        output = ""
+        postbox = Plugin[:gtk].widgetof(opt.widget).widget_post
+        message = postbox.buffer.text.split("\n")
+        message.each do |line|
+            output += line + "！（" + line + "！）\n"
+        end
+        output += "ﾝﾏｯ！"
+
+        if send_now then
+            world, = Plugin.filtering(:world_current, nil)
+            compose(world, body:output)
+            postbox.buffer.text = ""
+        else
+            postbox.buffer.text = output
+        end
+    end
+end
